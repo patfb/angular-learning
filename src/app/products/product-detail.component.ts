4e7b0bcfffd3729ace76f9eb9e9939f875593e94
Patrick <patrick.f.boyne@gmail.com>
2018-04-22 19:00:49 -0400

    add back button that goes to route

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'pm-product-detail',
@@ -7,7 +8,28 @@ import { IProduct } from './product';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-  constructor() {}
+  pageTitle: string = 'Product Detail';
+  product: IProduct;
 
-  ngOnInit() {}
+  constructor(private _route: ActivatedRoute, private _router: Router) {}
+
+  ngOnInit() {
+    let id = +this._route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      productId: id,
+      productName: 'Leaf Rake',
+      productCode: 'GDN-0011',
+      releaseDate: 'March 19, 2016',
+      description: 'Leaf rake with 48-inch wooden handle.',
+      price: 19.95,
+      starRating: 3.2,
+      imageUrl:
+        'http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png'
+    };
+  }
+
+  onBack(): void {
+    this._router.navigate(['/products']);
+  }
 }
