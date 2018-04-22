6ee43add87f64d1f2ef79d0ceceffa43c2595f4c
Patrick <patrick.f.boyne@gmail.com>
2018-04-22 14:21:59 -0400

    implement service that uses observables to subscribe to http requests

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
+  errorMessage: string;
 
   _listFilter: string;
 
@@ -47,7 +48,9 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.products = this._productService.getProducts();
-    this.filteredProducts = this.products;
+    this._productService.getProducts().subscribe(products => {
+      this.products = products;
+      this.filteredProducts = this.products;
+    }, error => (this.errorMessage = <any>error));
   }
 }
